515285816f09ed2ca9ded06712dcaaff588bab8a
Trevor Turk <trevorturk@yahoo.com>
2009-05-08 17:14:42 -0500

    basic users database, automatically populated, using sequel and sqlite in memory for the moment

--- app.rb
+++ app.rb
@@ -2,8 +2,30 @@
 
 require 'rubygems'
 require 'sinatra'
+require 'sequel'
+
+#
+# DATABASE
+#
+
+DB = Sequel.sqlite
+
+DB.create_table :users do
+  primary_key :id
+  String :login
+end
+
+DB[:users].insert(:login => 'geoff')
+DB[:users].insert(:login => 'scott')
+DB[:users].insert(:login => 'trevor')
+DB[:users].insert(:login => 'zach')
+
+#
+# ACTIONS
+#
 
 get '/' do
+  @users = DB[:users].all
   erb :home
 end
 
