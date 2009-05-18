15ddb7effff04b4f9de6800b7fb0837a083afaa2
Trevor Turk <trevorturk@yahoo.com>
2009-05-18 15:48:22 -0500

    improve users/show tests

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -6,5 +6,12 @@ class UsersControllerTest < ActionController::TestCase
     get :show, :login => User.make.login
     assert_response :success
   end
+  
+  test "show with tweets" do
+    u = User.make
+    Tweet.make(:user_id => u.id)
+    get :show, :login => u.login
+    assert_response :success
+  end
 
 end
