cc10aca1f72c489d5550935d73f5af396be68bb7
Trevor Turk <trevorturk@yahoo.com>
2009-05-20 16:16:40 -0500

    add basic login, always logged in as trevor, show tweets, feed, followings, and followers on users/show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,34 @@ class ApplicationController < ActionController::Base
   helper :all
   protect_from_forgery
   filter_parameter_logging :password, :password_confirmation
+  
+  # Returns true or false if the user is logged in.
+  # Preloads @current_user with the user model if they're logged in.
+  def logged_in?
+    !!current_user
+  end
+  helper_method :logged_in?
+  
+  # Accesses the current user from the session.
+  # Future calls avoid the database because nil is not equal to false.
+  def current_user
+    @current_user ||= (login_from_session || login_as_trevor) unless @current_user == false
+  end
+  helper_method :current_user
+  
+  # Store the given user id in the session.
+  def current_user=(new_user)
+    session[:user_id] = new_user ? new_user.id : nil
+    @current_user = new_user || false
+  end
+  
+  # Called from #current_user.  First attempt to login by the user id stored in the session.
+  def login_from_session
+    self.current_user = User.find_by_id(session[:user_id]) if session[:user_id]
+  end
+  
+  def login_as_trevor
+    self.current_user = User.find_by_login!('trevor')
+  end
+  
 end
--- lib/tasks/populate.rake
+++ lib/tasks/populate.rake
@@ -5,16 +5,20 @@ namespace :db do
     Rake::Task['db:schema:load'].invoke
     
     puts 'Creating Users...'
-    User.create(:login => 'geoff')
-    User.create(:login => 'scott')
-    User.create(:login => 'trevor')
-    User.create(:login => 'zach')
+    geoff = User.create(:login => 'geoff')
+    scott = User.create(:login => 'scott')
+    trevor = User.create(:login => 'trevor')
+    zach = User.create(:login => 'zach')
 
     puts 'Creating Tweets...'
     Tweet.create(:body => "i'm hungry... again...") {|r| r.user = User.find_by_login('geoff')}
     Tweet.create(:body => "it's too cold in the office :(") {|r| r.user = User.find_by_login('scott')}
     Tweet.create(:body => "let's drink some whiskey?") {|r| r.user = User.find_by_login('trevor')}
     Tweet.create(:body => "couchdb rulez!") {|r| r.user = User.find_by_login('zach')}
-
+    
+    puts 'Creating Follows'
+    geoff.follow(scott)
+    trevor.follow(zach)
+    
   end
 end
