28bb0d8260fadd0d316b7781ef32eafc2227e438
Trevor Turk <trevorturk@yahoo.com>
2009-05-26 17:57:18 -0500

    fix :include =>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find_by_login! params[:login], :include => [:tweets, :followings, :followers]
-    @feed_items = @user.feed_items :include => :tweets
+    @feed_items = @user.feed_items.all :include => [:tweet, :tweet_user]
   end
   
 end
