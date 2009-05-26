fa2f7d33a37b9e27574a6cbeeeaf4eb72bf756ea
Trevor Turk <trevorturk@yahoo.com>
2009-05-26 17:29:26 -0500

    show feed_items on users/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find_by_login! params[:login], :include => [:tweets, :followings, :followers]
+    @feed_items = @user.feed_items :include => :tweets
   end
   
 end
