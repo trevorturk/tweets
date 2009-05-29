60ea170f8c62e90e97b7e5debf07eb8d20cfc477
Trevor Turk <trevorturk@yahoo.com>
2009-05-29 16:21:22 -0500

    update readme, remove unnecessary option in follow model

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,8 +2,8 @@ class Follow < ActiveRecord::Base
   
   attr_accessible
   
-  belongs_to :follower, :foreign_key => :follower_id, :class_name => 'User'
-  belongs_to :following, :foreign_key => :following_id, :class_name => 'User'
+  belongs_to :follower, :class_name => 'User'
+  belongs_to :following, :class_name => 'User'
   
   validates_presence_of :follower_id
   validates_presence_of :following_id
